570c8c105a06a799065f178fc5d7f27a6e6aae3d
charlizeaponte <charlizeaponte@yahoo.com>
2024-01-27 20:27:37 -0500

    chapter4demo

---
+++ app/src/main/java/edu/quinnipiac/myconstraintlayout/MainActivity.kt
@@ -0,0 +1,16 @@
+/**
+ * @author Charlize Aponte
+ * Date: 1/27/24
+ * Assignment: This program shows how to use constraints layout with buttons
+ */
+package edu.quinnipiac.myconstraintlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
